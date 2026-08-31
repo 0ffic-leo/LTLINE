1f075ea7f9b7d97e4e1be97abbec452bc3caf38a
0ffic-leo <tahirajleonard@gmail.com>
2026-08-31 18:14:29 +0200

    Update Supabase anonKey in configuration

--- web-app/supabase-config.js
+++ web-app/supabase-config.js
@@ -3,5 +3,5 @@
 // NEVER place a service_role/secret key in this file.
 window.LTLINE_SUPABASE = {
   url: 'https://tlikdvhdwlhdcmkgrevo.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRsaWtkdmhkd2xoZGNta2dyZXZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxODUxMDYsImV4cCI6MjEwMzc2MTEwNn0.BOgnw3UQEfYvpAbpBrvKWwFDCxLm1uUjXp0V2BIpl7A'
+  anonKey: 'sb_publishable_O2SJl97F0SEzkWXNV3aVKw_RBZTGuak'
 };
